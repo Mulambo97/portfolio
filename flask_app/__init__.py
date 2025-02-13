2c76e4539f37fa271ff656bb2cfa019192faa06d
M M <mm@Ms-MacBook-Pro.local>
2025-02-13 03:28:52 -0500

    create a resume page and focus on retriving data from db and create table

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -17,7 +17,11 @@ def create_app():
     app.config["TEMPLATES_AUTO_RELOAD"] = True
     app.jinja_env.auto_reload = True
     app.jinja_env.cache = None
-    
+
+    from .utils.database.database import database
+    db = database()
+    db.createTables(purge=True)  # Ensure consistent indentation here
+
     with app.app_context():
         from . import routes  # Import routes inside app context
     return app
